91a697776a95cfbe6982e3929b11e1cc2707e48d
Eric <qianchengsijinmi@126.com>
2018-01-17 01:20:14 +0800

    delete bitbucket-pipelines.yml for github

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡¡¡™£¢∞§§∞§¶•ªº≠œ∑´´††¥¥¥¨¨¨ˆˆˆøøˆøππππππøπππππππøøˆˆ¨ˆπππππππππππ“““““““‘‘‘«««‘‘‘‘‘“æææææ…¬˚∆˙©∂∂ßßåß≈≈çç∂çç√∫˜≤≤≤≤≤≤≤≤≤≤≤hala, mundo!!!    hello, world! This is one of my APPs!!!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!!!"
+  end
 end
